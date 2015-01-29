934fa1992b34b6ed7ac34da6ad19d9c26d9bb52a
Rick Beerendonk <rick@beerendonk.com>
2015-01-29 09:00:19 +0100

    Build script now creates build directory if it doesn't exist.

--- Extra 05. IE8 and Virtual Table/jsx-compile-win.bat
+++ Extra 05. IE8 and Virtual Table/jsx-compile-win.bat
@@ -1,1 +1,12 @@
+@echo off
+
+set buildDir=build
+if not exist %buildDir% (mkdir %buildDir%)
+
 jsx --watch --extension jsx ./src ./build
+
+if ERRORLEVEL 1 (
+echo. 
+echo Script uses React Tools.
+echo Install: npm install -g react-tools
+)
