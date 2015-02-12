a89c81454d43e997cc535f9d7559c5fda7a4777e
Rick Beerendonk <rick@beerendonk.com>
2015-02-12 16:19:10 +0100

    Add "Extra 05. IE8"

---
+++ Extra 05. IE8/src/main.jsx
@@ -0,0 +1,12 @@
+﻿var HelloReact = React.createClass({
+	render: function () {
+		return (
+			<h1>Hello React!</h1>
+		);
+	}
+});
+
+React.render(
+	<HelloReact />,
+	document.getElementById('app')
+);
